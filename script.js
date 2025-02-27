6b7dad383c9cd9ae611b2d1f97ac2f9e4386b83a
Ayush Dwivedi <dwivediayush028@gmail.com>
2025-02-27 19:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-//your JS code here. If required.
+document.getElementById("enterBtn").addEventListener("click", function() {
+            const statusElement = document.getElementById("status");
+            const newElement = document.createElement("h1");
+            newElement.id = "status";
+            newElement.textContent = "Entered Metaverse";
+            statusElement.replaceWith(newElement);
+});
